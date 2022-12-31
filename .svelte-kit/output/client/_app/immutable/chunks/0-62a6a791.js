ca70b359a5822f1dcc2dc8af488306e11547cb41
abilaro <abilaro@pm.me>
2022-12-31 19:51:06 +0100

    removed binaries and deps

--- .svelte-kit/output/client/_app/immutable/chunks/0-62a6a791.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-3784003b.js";export{t as component};
--- .svelte-kit/output/client/_app/immutable/chunks/2-d97433ef.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-c3626082.js";export{t as component};
