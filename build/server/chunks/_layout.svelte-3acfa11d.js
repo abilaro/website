7d1c94479411bf8f780e562e524ecf699906023e
abilaro <abilaro@pm.me>
2022-12-31 19:48:55 +0100

    added gitignore

--- build/server/chunks/_layout.svelte-3acfa11d.js
+++
@@ -1,8 +0,0 @@
-import { c as create_ssr_component } from './index-1d92b199.js';
-
-const Layout = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<div style="${"background: #CCC"}">${slots.default ? slots.default({}) : ``}</div>`;
-});
-
-export { Layout as default };
-//# sourceMappingURL=_layout.svelte-3acfa11d.js.map
